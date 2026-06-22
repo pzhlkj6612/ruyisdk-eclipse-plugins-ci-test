9e751e49d31ca8dc459fd34761cc40f1f147dee5
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2026-06-22 23:22:44 +0000

    fix(promotion): imports
    Signed-off-by: Mozi <29089388+pzhlkj6612@users.noreply.github.com>

--- plugins/org.ruyisdk.promotion/src/org/ruyisdk/promotion/PromotionStartup.java
+++ plugins/org.ruyisdk.promotion/src/org/ruyisdk/promotion/PromotionStartup.java
@@ -6,6 +6,7 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.ruyisdk.core.util.PluginLogger;
 import org.ruyisdk.promotion.views.WebsiteView;
 
 /**
